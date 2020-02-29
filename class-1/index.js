c195e87e134d8c567201f82c3f5e50120a3a7216
sntwandy <sntwandy@gmail.com>
2020-02-29 04:58:38 -0400

    Class one

---
+++ class-1/index.js
@@ -0,0 +1,16 @@
+/* It's my first Hello World in Javascript, with the function
+console.log()  
+console.log('Hello world!');
+*/
+
+/*Variables:*/
+/*var name = 'Megan' , lastName = 'Herrera'*/
+var name = 'Megan'
+var lastName = 'Herrera'
+console.log('Hola ' + name + ' ' + lastName)
+
+var age = 20
+console.log(name + ' ' + lastName + ' has ' + age + ' years old')
+
+/* The variables in js doens't have a specific type */
+
