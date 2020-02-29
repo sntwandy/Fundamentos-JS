f8d220b8aeeae85929ef9660044225b04b946bac
sntwandy <sntwandy@gmail.com>
2020-02-29 16:59:47 -0400

    Class 4 functions

---
+++ class-4/index.js
@@ -0,0 +1,20 @@
+/* Functions */
+var name = 'Megan', age = 20
+
+/* Without parameters */
+function printName()
+{
+    console.log(`${name} has ${age} years old`)
+}
+
+/* Calling the function */
+printName()
+
+
+/* Using parameters */
+function printData(nPerson, aPerson)
+{
+    console.log(`${nPerson} has ${aPerson} years old`)
+}
+/* Calling the function */
+printData('Megan', 20)
