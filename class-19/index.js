1f6a3de3e1caf9029cc86c88f38c8c80dbe8030b
sntwandy <sntwandy@gmail.com>
2020-03-05 11:24:01 -0400

    Class 19 array, function map

---
+++ class-19/index.js
@@ -0,0 +1,55 @@
+/* Array Map */
+// Map function always return a new object or new array, if you dont modify the elements from original element.
+
+let megan = {
+    name: 'Megan',
+    lastName: 'Herrera',
+    heigth: 1.70
+}
+
+let zeus = {
+    name: 'Zeus',
+    lastName: 'Santana',
+    heigth: 1.84
+}
+
+let wandy = {
+    name: 'Wandy',
+    lastName: 'Santana',
+    heigth: 1.80
+}
+
+// OUR ARRAY
+let people =  [megan, zeus, wandy]
+
+// FUNCTIONS
+const isHeigth = ({ heigth }) => heigth >= 1.8
+// const notHeigth = ({ heigth }) => heigth < 1.8
+const peopleCms2 = people => ({ // Using parentesis you can return a object without return {}
+    // return a new object
+    ...people,
+    heigth: people.heigth * 100 
+})
+
+// USING FILTER
+let notHeigthPeople = people.filter(function ({ heigth }) {
+    return heigth < 1.8
+}) 
+let heigthPeople = people.filter(isHeigth)
+
+// USING MAP
+let peopleCms1 = people.map(peopleCms2)
+/* let peopleCms = people.map(function (people){
+    return{
+        //New object
+        ...people,
+        heigth: people.heigth * 100
+    }
+}) */
+
+// OUTPUT
+console.log(people)
+// console.log(heigthPeople)
+// console.log(notHeigthPeople)
+// console.log(peopleCms)
+console.log(peopleCms1)
