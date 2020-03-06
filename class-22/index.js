eb7f7c18db0bdb882199d358af25b0be43e6122a
sntwandy <sntwandy@gmail.com>
2020-03-06 11:51:26 -0400

    Class 22 arrow functions with objects and using this

---
+++ class-22/index.js
@@ -0,0 +1,36 @@
+/* Classes Prototypes */
+
+// Creating our prototype 'person'.
+function person (name, lastName, heigth){
+    this.name = name  // this. is the reference to the object.
+    this.lastName = lastName
+    this.heigth = heigth
+}
+
+// To create a atribute of say hi. with a function inside the prototype person.
+person.prototype.hi = function (){
+    console.log(`Hi, my name is ${this.name} ${this.lastName}`)
+}
+
+person.prototype.isHeigth = () => {
+    return this.heigth > 1.8 // Undifined, because this is not this from the object is from window, the global object. The correct way is using function
+}
+
+/* person.prototype.isHeigth = function (){
+    let limitHeigth = 1.8
+    if(this.heigth >= limitHeigth){
+        console.log(`${this.name} is a heigth person.`)
+    } 
+    else if(this.heigth < limitHeigth){
+        console.log(`${this.name} is NOT a heigth person.`)
+    }
+} */
+
+let megan = new person('Megan', 'Herrera', 1.78)
+let wandy = new person('Wandy', 'Santana', 1.82)
+
+wandy.hi()
+megan.hi()
+
+megan.isHeigth()
+wandy.isHeigth()
